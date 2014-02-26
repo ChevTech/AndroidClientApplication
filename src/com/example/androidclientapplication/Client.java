8ba1a4edab7781330796bf83749cc0f8c17a4fb6
Brian Kane <bekane05@gmail.com>
2014-02-25 21:54:26 -0500

    Update Client.java
    Cleaned up and added comments to the existing code

--- src/com/example/androidclientapplication/Client.java
+++ src/com/example/androidclientapplication/Client.java
@@ -1,6 +1,5 @@
 package com.example.androidclientapplication;
 
- 
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
@@ -17,33 +16,54 @@ import android.view.View.OnTouchListener;
 import android.widget.Button;
 import android.widget.EditText;
 
-// Main Activity
+/** Main Activity
+
+    Represents the first activity a user sees. The user enters the IP-address
+    of the server running on the Raspberry Pi and the port number that the
+    server is listening at.
+    
+    Presents a simple UI providing text views for the user to provide the IP-address
+    and port number as well as a button whose action method attemps to connect to the server.
+    If connection is successful, the second activity starts, otherwise displays an error message
+    and asks the user to re-enter the IP-address and port number.
+    
+    */
 public class Client extends Activity {
+	
 	private Socket socket;
 	private PrintWriter out;
-    private static final int SERVERPORT = 12355;
-    private static final String SERVER_IP = "10.32.95.167";
- 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);  
-        
-        new Thread(new ClientThread()).start();
+	private static final int SERVERPORT = 12355;
+	private static final String SERVER_IP = "10.32.95.167"; // IP-address of RPi server
+	
+	// Initialize the activity
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.main); // specifies layout resource defining the UI
+	        
+	        new Thread(new ClientThread()).start();
        	}
- 
-    /* Called when the user clicks one of the four directional buttons */
-    
+       	
+       	/* Called when the user clicks one of the four directional buttons */
 	public void buttonAction(View view) throws IOException {
+		
+		// Assigns the four directional buttons to Button objects
 		Button upButton = (Button) findViewById(R.id.upButton);
 		Button leftButton = (Button) findViewById(R.id.leftButton);
 		Button rightButton = (Button) findViewById(R.id.rightButton);
 		Button downButton = (Button) findViewById(R.id.downButton);
 		
+		// Interface definition for a callback to be invoked when a touch event is dispatched to this view
+		// * Need separate onTouchListeners for each button
 		upButton.setOnTouchListener(new OnTouchListener() {
+			
+			// Function called when a touch event is dispatched to a view
 			@Override
-			public boolean onTouch(View v, MotionEvent event) {	
+			public boolean onTouch(View v, MotionEvent event) {
+				
 				switch(event.getAction()){
+					
 					case MotionEvent.ACTION_DOWN:
 						out.println("Up-P");
 						return true;
@@ -59,9 +79,12 @@ public class Client extends Activity {
 		});
 		
 		leftButton.setOnTouchListener(new OnTouchListener() {
+			
 			@Override
-			public boolean onTouch(View v, MotionEvent event) {	
+			public boolean onTouch(View v, MotionEvent event) {
+				
 				switch(event.getAction()){
+					
 					case MotionEvent.ACTION_DOWN:
 						return true;
 					case MotionEvent.ACTION_MOVE:
@@ -76,9 +99,12 @@ public class Client extends Activity {
 		});
 		
 		rightButton.setOnTouchListener(new OnTouchListener() {
+			
 			@Override
-			public boolean onTouch(View v, MotionEvent event) {	
+			public boolean onTouch(View v, MotionEvent event) {
+				
 				switch(event.getAction()){
+					
 					case MotionEvent.ACTION_DOWN:
 						return true;
 					case MotionEvent.ACTION_MOVE:
@@ -93,9 +119,12 @@ public class Client extends Activity {
 		});
 		
 		downButton.setOnTouchListener(new OnTouchListener() {
+			
 			@Override
-			public boolean onTouch(View v, MotionEvent event) {	
+			public boolean onTouch(View v, MotionEvent event) {
+				
 				switch(event.getAction()){
+					
 					case MotionEvent.ACTION_DOWN:
 						return true;
 					case MotionEvent.ACTION_MOVE:
@@ -109,25 +138,32 @@ public class Client extends Activity {
 			}
 		});
 	}
- 
-   class ClientThread implements Runnable {
-	   @Override
-       public void run() {
-		   try {
-                InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
- 
-                socket = new Socket(serverAddr, SERVERPORT);
- 
-            } catch (UnknownHostException e1) {
-                e1.printStackTrace();
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
+	
+	// Private inner class to set up ClientThread
+	private class ClientThread implements Runnable {
+		
+		/** Starting the thread causes the run method to be called in a separately executing thread.
+		    Function attempts to creates a stream socket and connect it to the user-specified port number
+		    at the user-specified IP address */
+		@Override
+       		public void run() {
+       			
+			try {
+		                InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+		 
+		                socket = new Socket(serverAddr, SERVERPORT);
+		 
+		        } catch (UnknownHostException e1) {
+		                e1.printStackTrace();
+		        } catch (IOException e1) {
+		                e1.printStackTrace();
+		        }
 		   
-	        try {
+	        	try {
 				out = new PrintWriter(new BufferedWriter(
 				        new OutputStreamWriter(socket.getOutputStream())),
 				        true);
+				        
 			} catch (IOException e) {
 				System.out.println("Problem with establishing OutputStream");
 				e.printStackTrace();
